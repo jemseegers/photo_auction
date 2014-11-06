349a86cef4ed42d6d6ee6a78198eaf136f09c797
jemseegers <jem.seegers@gmail.com>
2014-11-06 17:10:02 -0500

    thank you professor miller

--- photo_auction_results.py
+++ photo_auction_results.py
@@ -11,6 +11,8 @@
 
 from bs4 import BeautifulSoup
 import requests
+import re
+p = re.compile('[0-9]+\s')
 
 all_lots = {
 		
@@ -22,8 +24,7 @@ with open("lots.txt") as lots_urls:
 		lot_html = lot_requests.text
 		soup = BeautifulSoup(lot_html)
 		#print(lot_html)
-
-
+		all_lots[each_lot] = {}
 
 		all_links = soup.find_all("div" , attrs = {"class" : "wide-right-column" })
 		for div_link in all_links:
@@ -32,10 +33,12 @@ with open("lots.txt") as lots_urls:
 			artist = div_link.find("h4")
 			artist = artist.text
 			#print(artist)
+			all_lots[each_lot]["artist"] = artist
 
 			title = div_link.find("h5")
 			title = title.text
 			#print(title)
+			all_lots[each_lot]["title"] = title
 
 		p_in_div = soup.find_all("div" , attrs = {"class" : "right-column" })
 		for p_link in p_in_div:
@@ -43,22 +46,26 @@ with open("lots.txt") as lots_urls:
 
 			price = p_link.find("p", attrs ={"class" : "price sold"})
 			price = price.text
-			print(price)
 
-import re
-p = re.compile('[0-9]\s')
-m = p.search(price)
-print(m)
+			m = p.search(price)
+
+			if m == None:
+				print("No interest")
+				all_lots[each_lot]["price"] = "Unsold"
+			else :
+				print(m.group(0))
+				#print(price)
+				all_lots[each_lot]["price"] = m.group(0)
 
 
 			#def only_money_or_unsold
 
 #i don't know how to make all the artists appear in the json file
 #it's only getting the second artist
-# import json
+import json
 
-# with open('artist_names.json' , 'w') as f:
-# 	f.write(json.dumps(artist))
+with open('all_lots.json' , 'w') as f:
+ 	f.write(json.dumps(all_lots))
 
 	
 
