8153080e086e833acd0456c92c5cc11118a90ee4
jemseegers <jem.seegers@gmail.com>
2014-11-13 17:47:43 -0500

    whoa so much progress

---
+++ 2011_may_sep_date_from_title.py
@@ -0,0 +1,19 @@
+import json
+import re
+p = re.compile('[0-9]{4}')
+
+with open("2011_may_results.json") as json_file:
+	json_data = json.load(json_file)
+	#print(json_data)
+
+	for a_work in json_data:
+		#print(a_work)
+		# if (json_data[a_work]["title"]):
+		if "title" in json_data[a_work]:
+			#print(json_data[a_work]['title'])
+
+			title = json_data[a_work]['title']
+			#print(title)
+			m = p.findall(title)
+			if len(m) > 0:
+				print(m[0])
