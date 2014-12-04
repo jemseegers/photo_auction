d7105e0e3afb5637dae9245ad2fd6e7cb688868c
jemseegers <jem.seegers@gmail.com>
2014-12-04 17:41:45 -0500

    yay matt helped make the csv file work

---
+++ 2011_may_decades_prices.py
@@ -0,0 +1,20 @@
+import json, csv
+
+dp_analysis = open('2011_may_results_with_dates.json')
+data = json.load(dp_analysis)
+dp_analysis.close()
+
+dp_analysis=csv.writer(open('dp_analysis.csv' , 'w'))
+
+for item in data:
+	if "decade" in data[item] and "price" in data[item]:
+		print(data[item]["decade"])
+		print(data[item]["price"])
+		print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+		dp_analysis.writerow([data[item]['decade'], data[item]['price']])
+	# print(data[item])
+	# print(data[item]["decade"])
+	
+
+# with open("2011_may_results.json") as json_file:
+# 	json_data = json.load(json_file)
