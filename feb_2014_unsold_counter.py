d2f29fa32e618e978dd2cb7c43de92abe3e17146
jemseegers <jem.seegers@gmail.com>
2014-12-07 18:48:39 -0500

    adding number unsold by decade counter thnx matt

---
+++ feb_2014_unsold_counter.py
@@ -0,0 +1,31 @@
+import csv
+
+all_decades = {}
+
+with open('feb_2014_dp_analysis.csv', 'r') as dpa:
+	reader = csv.reader(dpa)
+
+	for row in reader:
+
+		decade = row[0]
+
+		price = row[1].strip()
+
+		if decade not in all_decades:
+			all_decades[decade] = {}
+			
+		if price not in all_decades[decade]:
+			all_decades[decade][price] = 1
+		
+		else:
+			all_decades[decade][price] = all_decades[decade][price] + 1
+
+dp_analysis_by_price=csv.writer(open('feb_2014_dp_analysis_by_price.csv' , 'w'))
+
+
+for key in sorted(all_decades):
+
+
+	for price_key in sorted(all_decades[key]):
+
+		dp_analysis_by_price.writerow([ key, price_key,  all_decades[key][price_key] ])
