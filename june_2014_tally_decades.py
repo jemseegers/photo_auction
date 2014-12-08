a0bf6d46ab3ffb4ac1d0d60122af92c00ef676bc
jemseegers <jem.seegers@gmail.com>
2014-12-07 20:12:13 -0500

    saving it for tmrw

--- june_2014_tally_decades.py
+++ june_2014_tally_decades.py
@@ -3,19 +3,30 @@ import collections
 
 counts = collections.Counter()
 
-# #lets define a dictonary to hold our results
-# tally_d = {}
+#lets define a dictonary to hold our results
+tally_d = {}
 
 with open('june_2014_dp_analysis.csv', 'r') as td:
 	csv_read = csv.reader(td)
 	sort_td = sorted(csv_read)
 	#print("the number of records", len(sort_td))
 	for row in sort_td:
-		if row[0].isdigit():
-			if row[0] in sort_td:
-				sort_td[row[0]] = + 1
-			else:
-				sort_td[row[0]] = 1
-		
-			print(row)
+		decade = row[0]
+
+		if decade not in tally_d:
+			tally_d[decade] = {}
+
+		if decade not in tally_d:
+			#set it to one because it has one hit sofar (the first one)
+			tally_d[decade] = 1
+		else:
+			#otherwise it already exists so just bump up the counter
+			tally_d[decade] = tally_d[decade] + 1
+
+get_it=csv.writer(open('june_2014_decade_tally.csv', 'w'))
+
+for key in sorted(tally_d):
+	for dec_num in sorted(tally_d[decade]):
+
+		get_it.writerow([key, dec_num])
 
