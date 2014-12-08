9b69620a1e719e480ff0cdb4ecdd1e8b19e75b45
jemseegers <jem.seegers@gmail.com>
2014-12-08 11:57:10 -0500

    work session with matt

--- june_2014_pxd_tally.py
+++ june_2014_pxd_tally.py
@@ -12,14 +12,19 @@ with open('june_2014_dp_analysis.csv', 'r') as td:
 	#print("the number of records", len(sort_td))
 	for row in sort_td:
 		decade = row[0]
+		price = row[1]
 
+		if price == "Unsold":
+			price = 0
+		price = int(price)
+		
 		if decade not in tally_d:
-			tally_d[decade] = 1
+			tally_d[decade] = price
 		else:
 			#otherwise it already exists so just bump up the counter
-			tally_d[decade] = tally_d[decade] + 1
+			tally_d[decade] = tally_d[decade] + price
 
-get_it=csv.writer(open('june_2014_decade_tally.csv', 'w'))
+get_it=csv.writer(open('june_2014_pxd_tally.csv', 'w'))
 
 for decade in sorted(tally_d):
 		get_it.writerow([decade, tally_d[decade]])
