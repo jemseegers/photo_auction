03abf7b885529c3f2f7da0855d3e32c7080854db
jemseegers <jem.seegers@gmail.com>
2014-12-08 13:58:30 -0500

    creating all the files for the other auctions

--- 2013_nov_pxd_totals.py
+++ 2013_nov_pxd_totals.py
@@ -3,13 +3,12 @@ import collections
 
 counts = collections.Counter()
 
-#lets define a dictonary to hold our results
 tally_d = {}
 
-with open('june_2014_dp_analysis.csv', 'r') as td:
+with open('2013_nov_dp_analysis.csv', 'r') as td:
 	csv_read = csv.reader(td)
 	sort_td = sorted(csv_read)
-	#print("the number of records", len(sort_td))
+	
 	for row in sort_td:
 		decade = row[0]
 		price = row[1]
@@ -17,14 +16,14 @@ with open('june_2014_dp_analysis.csv', 'r') as td:
 		if price == "Unsold":
 			price = 0
 		price = int(price)
-		
+
 		if decade not in tally_d:
 			tally_d[decade] = price
 		else:
-			#otherwise it already exists so just bump up the counter
+			
 			tally_d[decade] = tally_d[decade] + price
 
-get_it=csv.writer(open('june_2014_pxd_tally.csv', 'w'))
+get_it=csv.writer(open('2013_nov_pxd_totals.csv', 'w'))
 
 for decade in sorted(tally_d):
 		get_it.writerow([decade, tally_d[decade]])
